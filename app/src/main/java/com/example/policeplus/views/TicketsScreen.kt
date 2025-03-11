b859fdc0f4b2564255997b007fe2e5d8f611ec91
nnul <salahxd99@gmail.com>
2025-03-11 21:58:40 +0100

    fixed a name spelling issue

--- app/src/main/java/com/example/policeplus/views/TicketsScreen.kt
+++ app/src/main/java/com/example/policeplus/views/TicketsScreen.kt
@@ -10,14 +10,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
